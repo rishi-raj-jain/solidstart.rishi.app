f899769668698733c74b8c9e15506902ce39c33d
Rishi Raj Jain <rjain@llnw.com>
2023-04-04 02:19:06 +0530

    path

--- edgio/build.cjs
+++ edgio/build.cjs
@@ -7,9 +7,9 @@ module.exports = async () => {
   const builder = new DeploymentBuilder()
   builder.clearPreviousBuildOutput()
   await builder.exec('npm run build')
-  const functionPath = `.vercel/output/functions/render.func/home/dist/index.cjs`
+  const functionPath = `.vercel/output/functions/render.func${process.cwd()}/dist/index.cjs`
   builder.addJSAsset(functionPath)
-  builder.writeFileSync(join(appDir, 'serverless'), functionPath)
+  builder.writeFileSync(join(builder.jsDir, 'serverless'), functionPath)
   await builder.build()
   builder.writeFileSync(join(builder.jsDir, '__backends__', 'package.json'), JSON.stringify({ type: 'commonjs' }))
 }

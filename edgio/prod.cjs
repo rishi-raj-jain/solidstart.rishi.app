07fce68763a2b4461f01c828fd5a3a8152d9f630
Rishi Raj Jain <rjain@llnw.com>
2023-04-04 01:57:03 +0530

    Update prod.cjs

--- edgio/prod.cjs
+++ edgio/prod.cjs
@@ -1,6 +1,9 @@
+const { fs } = require('fs')
+const { join } = require('path')
 const { createServer } = require('http')
 
 module.exports = async (port) => {
   process.env.PORT = port
-  createServer(require('../.vercel/output/functions/render.func/Users/rishirajjain/Desktop/solidstart.rishi.app/dist/index.cjs')).listen(port)
+  const functionPath = fs.readFileSync(join(process.cwd(), 'serverless'), 'utf8')
+  createServer(require(`../${functionPath}`)).listen(port)
 }
